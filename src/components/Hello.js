bc9ad06905e9131084d901c69c80d39b8b87aea3
LoveGT <646101390@qq.com>
2021-12-06 17:14:54 +0800

    ReactCli lesson52

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import "./Hello.css"
+export default class Hello extends  Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Hello World2</h1>
+      </div>
+    )
+  }
+}
