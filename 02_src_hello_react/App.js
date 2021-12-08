91b6c273beefb4f8a0137692f5e4506e5a8f9d07
LoveGT <646101390@qq.com>
2021-12-08 18:07:50 +0800

    hello react

--- 02_src_hello_react/App.js
+++ 02_src_hello_react/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+
 import Hello from "./components/Hello/Hello";
+import Welcome from "./components/Welcome/Welcome";
+
 export default class App extends Component {
   render() {
-    return <Hello></Hello>;
+    return (
+      <div>
+        <Hello></Hello>
+        <Welcome></Welcome>
+      </div>
+    );
   }
 }
