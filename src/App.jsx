75774dab16bf94d84963be284a18319272009026
gaotao <646101390@qq.com>
2021-12-16 02:10:25 +0800

    实现react路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
-import List from './components/List'
-import Search from './components/Search'
+import React, { Component } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import About from "./components/About";
+import Home from "./components/Home";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Search />
-        <List />
+      <div className="page-box">
+        <div className="page-header">
+          <h2>React Router Demo</h2>
+        </div>
+        <div className="content">
+          <div className="list-group">
+            <NavLink to="/about">ABOUT</NavLink>
+            <NavLink to="/home">HOME</NavLink>
+          </div>
+          <div className="content-text">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
