da5a37c4e98ed27ea3be2a1801a2a92a8911abd8
LoveGT <646101390@qq.com>
2021-12-17 18:15:12 +0800

    antd的初体验及按需配置

---
+++ 11_src_二级路由/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import MyNavLink from "./components/MyNavLink";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="page-box">
+        <div className="page-header">
+          <h2>React Router Demo</h2>
+        </div>
+        <div className="content">
+          <div className="list-group">
+            {/* <NavLink to="/about">ABOUT</NavLink>
+            <NavLink >HOME</NavLink> */}
+            <MyNavLink to="/about">About</MyNavLink>
+            <MyNavLink to="/home">Home</MyNavLink>
+            {/* <Test to="/home">Test</Test> */}
+          </div>
+          <div className="content-text">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/home" element={<Home />} />
+              {/* <Route path="*" element={<Navigate to="Home" />}></Route> */}
+            </Routes>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
