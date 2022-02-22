5466e30e34ee7343e18d9a7e9baeed289629b2f8
LoveGT <646101390@qq.com>
2022-02-22 18:43:46 +0800

    cinema

--- src/components/FilmDetail/index.jsx
+++ src/components/FilmDetail/index.jsx
@@ -4,7 +4,7 @@ export default class FilmDetail extends Component {
   render() {
     const { filmDetails } = this.props
     return (
-      <div className='film-detail'>{filmDetails}</div>
+      <div className='film-detail'>{filmDetails?  filmDetails : "暂无内容"}</div>
     )
   }
 }
